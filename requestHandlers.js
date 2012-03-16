84fc7fad965688c4f6a8630671755ab879c0848d
Milo Mordaunt <milo@Awesomearch-Z560.localdomain>
2012-03-16 17:43:10 +0000

    changed just about everything. Added loads. Revolutionised the world. Toppled a communist regime.

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,35 +2,23 @@ var queryString = require("querystring"),
     fs = require("fs"),
     formidable = require("formidable");
 
-function start(res, postData) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
-  var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" '+
-    'content="text/html; charset=UTF-8" />'+
-    '</head>'+
-    '<body>'+
-    '<form action="/upload" enctype="multipart/form-data" '+
-    'method="post">'+
-    '<input type="file" name="upload">'+
-    '<input type="submit" value="Upload file" />'+
-    '</form>'+
-    '</body>'+
-    '</html>';
-
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(body);
-    res.end();
+  fs.readFile("./site/index.html", function(err, data) {
+      if (err) throw err;
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write(data);
+      response.end();
+  });
 }
 
 function upload(response, request) {
   console.log("Request handler 'upload' was called.");
 
   var form = new formidable.IncomingForm();
-  console.log("about to parse");
+
   form.parse(request, function(error, fields, files) {
-    console.log("parsing done");
 
     /* Possible error on Windows systems:
        tried to rename to an already existing file */
@@ -47,7 +35,7 @@ function upload(response, request) {
   });
 }
 
-function show(response) {
+function showImage(response) {
   console.log("Request handler 'show' was called.");
   fs.readFile("/tmp/test.png", "binary", function(error, file) {
     if(error) {
@@ -61,6 +49,17 @@ function show(response) {
     }
   });
 }
+
+function show(response) {
+    showImage(response);
+}
+
+function favi() {
+    fs.readFile("../favicon.ico", "binary", function(err, data) {
+	if (err) throw err;
+	return data;
+    });
+};
 
 exports.start = start;
 exports.upload = upload;
--- router.js
+++ router.js
@@ -1,13 +1,35 @@
-function route(handle, pathname, res, req) {
+var fs = require("fs"),
+    path = require("path");
+
+function route(handle, pathname, response, request) {
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-	handle[pathname](res, req);
+	handle[pathname](response, request);
     } else {
-	console.log("No request handler fount for " + pathname);
-	res.writeHead(404, {"Content-Type": "text/plain"});
-	res.write("404 Not fount");
-	res.end();
+	var filename = path.join(process.cwd() + "/site", pathname);
+	path.exists(filename, function(exists) {
+	    if(!exists) {
+		response.writeHead(404, {"Content-Type": "text/plain"});
+		response.write("404 Not fount");
+		response.end();
+		return;
+	    }
+
+	    fs.readFile(filename, "binary",  function(err, data) {
+		if(err) {
+		    response.writeHead(500, {"Content-Type": "text/plain"});
+		    response.write(err + "\n");
+		    response.end();
+		    return;
+		}
+		
+		response.writeHead(200);
+		response.write(data, "binary");
+		response.end();
+	    });
+	});
     }
 }
+
 
 exports.route = route;
